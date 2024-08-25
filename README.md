2996724a846b3981e8a0187f48ef9e8aecc2fca1
Nick7OlFox <107397112+Nick7OlFox@users.noreply.github.com>
2024-08-25 13:10:41 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # online-vote
+
+# Tech stack
+-<b>Node.js</b> <i>20.1.0</i>
+
+# Assumptions
+- There cannot be two polls running at the same time
---
+++ backend/polling-backend/src/test/java/com/exercise/polling_backend/PollingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.polling_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
