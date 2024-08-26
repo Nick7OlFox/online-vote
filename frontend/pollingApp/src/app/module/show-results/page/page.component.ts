9de538c829c6a9cb304062c44d7cfdb7cdee6d4e
Nick7OlFox <107397112+Nick7OlFox@users.noreply.github.com>
2024-08-26 13:58:58 +0100

    Visual Changes

--- frontend/pollingApp/src/app/module/show-results/page/page.component.ts
+++ frontend/pollingApp/src/app/module/show-results/page/page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { globals } from '../../../app.component';
 
 @Component({
@@ -6,7 +6,26 @@ import { globals } from '../../../app.component';
   templateUrl: './page.component.html',
   styleUrl: './page.component.css'
 })
-export class PageComponent {
+export class PageComponent implements OnInit {
+
   message = globals.message;
   options = globals.options;
+  totalNumberOfVotes = 0;
+
+  ngOnInit() {
+    if (this.options != undefined) {
+      // Calculate total number of votes
+      this.options.forEach((element: { numberOfVotes: number; }) => {
+        this.totalNumberOfVotes += element.numberOfVotes;
+      });
+
+      // Calculate percentage for each option
+      this.options.forEach((element: any) => {
+        element.percentage = Math.trunc((element.numberOfVotes / this.totalNumberOfVotes) * 100);
+      });
+
+      // Sort options by percentage in descending order
+      this.options.sort((a: any, b: any) => b.percentage - a.percentage);
+    }
+  }
 }
